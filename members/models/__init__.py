f391f6d3b1eee4521faa8e97888822a822e0e36a
Andrew <andrew.sh@razortheory.com>
2017-11-24 15:30:40 +0300

    Added migrations

---
+++ members/models/__init__.py
@@ -0,0 +1,4 @@
+from members.models.members import MemberManager, ClientsManager, ManagersManager, Member
+from members.models.currencies import Currency
+from members.models.bank_accounts import BankAccount
+from members.models.bank_cards import BankCard
